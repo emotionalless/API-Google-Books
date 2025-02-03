4a0308c07e38bc9a67e77000451f1d670625ce66
kanade_myers <146585768+emotionalless@users.noreply.github.com>
2025-02-03 11:01:05 +0700

    Delete API Google Books directory

--- API Google Books/src/index.js
+++
@@ -1,7 +0,0 @@
-import './css/css-reset.css'
-import './css/fonts.css'
-import './css/media-width.css'
-import './css/styles.css'
-
-const jsImport1 = require('./js/books-api')
-const jsImport2 = require('./js/slider')
