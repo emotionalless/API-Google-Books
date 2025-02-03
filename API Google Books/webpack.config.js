328eda5104175d034450ba195f307eb4101a7893
kanade_myers <146585768+emotionalless@users.noreply.github.com>
2025-02-03 11:04:49 +0700

    Add files via upload

---
+++ API Google Books/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        filename: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name][contenthash].js',
+        assetModuleFilename: '[name][ext]',
+        clean: true
+    },
+    performance: {
+        hints: false,
+        maxAssetSize: 512000,
+        maxEntrypointSize: 512000
+    },
+    devServer: {
+        port: 8080,
+        compress: true,
+        hot: true,
+        static: {
+            directory: path.join(__dirname, 'dist')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'Bookshop',
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: path.resolve('src/img'), to: path.resolve(__dirname, 'dist', 'img') }
+            ],
+          })
+    ]
+}
